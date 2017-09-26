46aee7f2955ad9ac385cf2e511c219e550fe3820
taddeimania <jtaddei@gmail.com>
2017-09-26 10:39:47 -0400

    Added secret model, can add to in memory array

--- src/main/java/io/joel/controllers/HomeController.java
+++ src/main/java/io/joel/controllers/HomeController.java
@@ -1,13 +1,31 @@
 package io.joel.controllers;
 
+import io.joel.models.Secret;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
+    private ArrayList<Secret> mySecrets = new ArrayList<>();
+
     @RequestMapping(value = "/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("secret", new Secret());
+        model.addAttribute("mySecrets", mySecrets);
         return "index";
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String index(@ModelAttribute Secret secret) {
+        System.out.println(secret);
+        mySecrets.add(secret);
+        return "redirect:/";
+    }
+
 }
