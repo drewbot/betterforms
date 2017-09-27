cde6275d781b6fc187a3fe70095fe494633643c9
taddeimania <jtaddei@gmail.com>
2017-09-27 09:43:19 -0400

    Added webjar for bootstrap css into project and index.html

--- src/main/java/io/joel/controllers/HomeController.java
+++ src/main/java/io/joel/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package io.joel.controllers;
 
 import io.joel.models.Secret;
+import io.joel.repositories.SecretRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,19 +14,20 @@ import java.util.ArrayList;
 @Controller
 public class HomeController {
 
-    private ArrayList<Secret> mySecrets = new ArrayList<>();
+    @Autowired
+    private SecretRepository secretRepo;
 
     @RequestMapping(value = "/")
     public String index(Model model) {
         model.addAttribute("secret", new Secret());
-        model.addAttribute("mySecrets", mySecrets);
+        model.addAttribute("mySecrets", secretRepo.findAll());
         return "index";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public String index(@ModelAttribute Secret secret) {
         System.out.println(secret);
-        mySecrets.add(secret);
+        secretRepo.save(secret);
         return "redirect:/";
     }
 
