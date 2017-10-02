37c6b9fcbb285bf3b3f7eb38aab4ee1c69b45ce3
taddeimania <jtaddei@gmail.com>
2017-10-02 09:23:25 -0400

    Adding confirm question to delete secret

--- src/main/java/io/joel/controllers/SecretController.java
+++ src/main/java/io/joel/controllers/SecretController.java
@@ -5,8 +5,10 @@ import io.joel.repositories.SecretRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SecretController {
@@ -21,4 +23,26 @@ public class SecretController {
         model.addAttribute("secret", mySecret);
         return "secret";
     }
+
+    @RequestMapping(value = "/secret/{id}", method = RequestMethod.POST)
+    public String secretDetailForm(@ModelAttribute Secret secret) {
+        secretRepo.save(secret);
+        return "redirect:/";
+    }
+
+    // Delete confirmation page
+    @RequestMapping(value = "/secret/{id}/delete")
+    public String secretDeleteConfirm(Model model,
+                                      @PathVariable("id") Long id) {
+        Secret secret = secretRepo.findOne(id);
+        model.addAttribute("secret", secret);
+        return "secretDeleteConfirm";
+    }
+
+    @RequestMapping(value = "/secret/{id}/delete", method = RequestMethod.POST)
+    public String secretDelete(@PathVariable("id") Long id) {
+        secretRepo.delete(id);
+        return "redirect:/";
+    }
+
 }
